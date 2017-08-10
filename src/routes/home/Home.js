a98ab74714c0e245687f408d5880f5935b6f2028
akhzan <rizal.akhzan@gmail.com>
2017-08-10 14:52:11 +0700

    add header

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -4,6 +4,12 @@ class Home extends Component {
   render() {
     return (
       <div className="home">
+	      <div className="home-header-container">
+	      	<div className="home-header">
+	      		<div className="home-name">Babubnb</div>
+	      		<div className="home-desc">Sewa babu-babu profesional untuk setiap pekerjaan dan kendala anda</div>
+	      	</div>
+	      </div>
       </div>
     );
   }
