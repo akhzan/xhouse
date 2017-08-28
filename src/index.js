705e2274d7571f869ae5db8b914e13e5536dac36
akhzan <rizal.akhzan@gmail.com>
2017-08-28 19:37:55 +0700

    add react router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import './style/index.css';
 import App from './routes/app/App';
 import registerServiceWorker from './registerServiceWorker';
 import injectTapEventPlugin from 'react-tap-event-plugin'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Router>
+		<Route path='/' component={App}>
+		</Route>
+	</Router>
+	, document.getElementById('root')
+);
 injectTapEventPlugin()
 registerServiceWorker();
