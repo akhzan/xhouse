d9e6d92abeb77631ffc46d880e550277de5da8a5
akhzan <rizal.akhzan@gmail.com>
2017-08-29 19:16:16 +0700

    initial component showcase

---
+++ src/components/showcase/Showcase.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Showcase extends Component {
+	constructor() {
+		super();
+	}
+
+	render() {
+		const { x } = this.props;
+		const img = { url:'https://a0.muscache.com/im/pictures/4913407/6ceaefb6_original.jpg'};
+	return (
+	  <div className="showcase">
+	  	  	<div></div>
+	  </div>
+	);
+	}
+}
+
+export default Showcase;
