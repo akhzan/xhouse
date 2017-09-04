b556b1b29393f8d6547e18aca49107f413181ee3
bagus hadi <bagus.hadi@ai.astra.co.id>
2017-09-04 23:32:04 +0700

    working on showcase

--- src/components/showcase/Showcase.js
+++ src/components/showcase/Showcase.js
@@ -9,18 +9,20 @@ class Showcase extends Component {
 		const { x } = this.props;
 		const img = { url:'https://a0.muscache.com/im/pictures/4913407/6ceaefb6_original.jpg'};
 	return (
-	  <div className="showcase">
-	  		<div className="showcase-card">
-	  			<img className="showcase-img" src={img.url} alt="x"></img>
-	  			<div className="showcase-card-title">Jakarta<span>----badge</span></div>
-	  			<div className="showcase-card-tagline">The best babu ever bro!</>
-  				<div><span>Mulai dari</span><b> Rp. 500.000</b></div>
-  				<div className="showcase-value">
-  					<div className="showcase-star">Star</div>
-  					<div className="showcase-review">Review</div>
-  				</div>
-	  		</div>
-	  </div>
+	  	<div className="showcase">
+			<div className="showcase-content">
+				<div className="showcase-card">
+					<img className="showcase-img" src={img.url} alt="x"></img>
+					<div className="showcase-card-title">Jakarta<span>----badge</span></div>
+					<div className="showcase-card-tagline">The best babu ever bro!</div>
+					<div><span>Mulai dari</span><b> Rp. 500.000</b></div>
+					<div className="showcase-value">
+						<div className="showcase-star">Star</div>
+						<div className="showcase-review">Review</div>
+					</div>
+				</div>
+			</div>
+	  	</div>
 	);
 	}
 }
